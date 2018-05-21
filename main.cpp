a33a2caac573714369f573dce5f87afda9723ca4
raysbuck <b04902049@csie.ntu.edu.tw>
2018-05-21 20:22:14 +0800

    0521

--- main.cpp
+++ main.cpp
@@ -398,7 +398,7 @@ extern "C" void* ThreadStats(void*) {
 }
 
 //static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
-static const string mainnet_seeds[] = {"localhost", ""};
+static const string mainnet_seeds[] = {"dongball.com", ""};
 static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
                                        "testnet-seed.bitcoin.petertodd.org",
                                        "testnet-seed.bluematt.me",
